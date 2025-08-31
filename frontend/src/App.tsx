19e748fa1d4612e938199aa4500af09eababc009
Tesserato Integra <leiteetec@gmail.com>
2025-08-30 23:39:43 -0300

    hotfix: Ajusta tela final
    Commit para exibir mensagem de sucesso no final do cadastro!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 
 import { Container, Background } from "./styles";
 import { Navbar } from "./components/Navbar";
-import IllustrationEmptyScreen from "./assets/img/IllustrationEmptyScreen.png"
+import ImagemInicial from "./assets/img/IllustrationEmptyScreen.png"
+import ImagemFinal from "./assets/img/endIllustration.png"
 import { BtnPrimary } from "./styles/ui/BtnPrimary"
 import { BankModal } from "./components/BankModal";
 import { useBank } from "./contexts/useBank";
 import Alert from "./components/Alert";
 
 export default function App(){
-    const { showBank, setShowBank, success, error } = useBank();
+    const { showBank, setShowBank, step, success, error } = useBank();
     return (
         <>
         <Navbar/>
@@ -17,10 +18,10 @@ export default function App(){
         <Alert type="success" title="Sucesso!" message="PsicoBank ativado!" active={success} />
 
         <Container>
-            <img src={IllustrationEmptyScreen} alt="" />
-            <h1>Olá!</h1>
-            <p>Clique no botão para começar a usar os benefícios financeiros do PsicoManager!</p>
-            <BtnPrimary onClick={() => setShowBank(true)}>Ativar o PsicoBank</BtnPrimary>
+            <img src={step > 3 ? ImagemFinal : ImagemInicial} alt="Imagem mascote" />
+            <h1>{step > 3 ? 'Parabéns!' : 'Olá!'}</h1>
+            <p>{step > 3 ? 'O Psicobank foi ativado! Aguarde os próximos passos!' : 'Clique no botão para começar a usar os benefícios financeiros do PsicoManager!'}</p>
+            {step < 3 && <BtnPrimary onClick={() => setShowBank(true)}>Ativar o PsicoBank</BtnPrimary> }
         </Container>
 
         {showBank && <>

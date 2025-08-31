b3109c4ddf080c34ec153ad7050aa9afc7a31419
Tesserato Integra <leiteetec@gmail.com>
2025-08-30 23:16:43 -0300

    Challenge completed!
    Projeto Finalizado
    Responsivo.
    Seguindo o protótipo Figma.
    Psicomanager > PsicoBank

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Container, Background } from "./styles";
+import { Navbar } from "./components/Navbar";
+import IllustrationEmptyScreen from "./assets/img/IllustrationEmptyScreen.png"
+import { BtnPrimary } from "./styles/ui/BtnPrimary"
+import { BankModal } from "./components/BankModal";
+import { useBank } from "./contexts/useBank";
+import Alert from "./components/Alert";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App(){
+    const { showBank, setShowBank, success, error } = useBank();
+    return (
+        <>
+        <Navbar/>
+
+        <Alert type="error" title="Atenção!" message="Os campos obrigatórios não foram preenchidos" active={error} />
+        <Alert type="success" title="Sucesso!" message="PsicoBank ativado!" active={success} />
+
+        <Container>
+            <img src={IllustrationEmptyScreen} alt="" />
+            <h1>Olá!</h1>
+            <p>Clique no botão para começar a usar os benefícios financeiros do PsicoManager!</p>
+            <BtnPrimary onClick={() => setShowBank(true)}>Ativar o PsicoBank</BtnPrimary>
+        </Container>
+
+        {showBank && <>
+            <BankModal />
+            <Background />
+        </>}
+        </>
+    )
 }
-
-export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { GlobalStyle } from './styles/global.ts';
+import { BankProvider } from './contexts/BankContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BankProvider>
+      <GlobalStyle />
+      <App />
+    </BankProvider>
   </StrictMode>,
 )
